706a615b48431c5e10fcff7e05d8e2fef80a7333
shuyan <shuyan434224591@gmail.com>
2020-11-04 18:39:25 +0800

    no message

--- 3.VIP课程/2020-10-19（class文件、类加载)/课件/jvm/src/GcTest.java
+++ 3.VIP课程/2020-10-19（class文件、类加载)/课件/jvm/src/GcTest.java
@@ -6,7 +6,7 @@ public class GcTest {
     public static void main(String[] args) {
         byte[] b1 = new byte[2 * _1MB];
         byte[] b2 = new byte[2 * _1MB];
-        byte[] b3 = new byte[2 * _1MB];
-        byte[] b4 = new byte[4 * _1MB];
+        //byte[] b3 = new byte[2 * _1MB];
+        byte[] b4 = new byte[5 * _1MB];
     }
 }
