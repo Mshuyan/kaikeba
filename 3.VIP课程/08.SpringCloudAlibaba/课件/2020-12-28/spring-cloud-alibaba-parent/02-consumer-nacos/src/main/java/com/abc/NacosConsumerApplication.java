e8644cdfac4dbc74e19618d07e9ea3a1166b0b17
shuyan <shuyan434224591@gmail.com>
2020-12-28 23:38:15 +0800

    no message

---
+++ 3.VIP课程/08.SpringCloudAlibaba/课件/2020-12-28/spring-cloud-alibaba-parent/02-consumer-nacos/src/main/java/com/abc/NacosConsumerApplication.java
@@ -0,0 +1,33 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class NacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerApplication.class, args);
+    }
+
+    /**
+     * 注入RestTemplate模板对象，用来发送http请求
+     * 作用相当于：<bean id='' class='org.springframework.web.client.RestTemplate'></bean>
+     *
+     * RestTemplate底层有：
+     *  HttpClient
+     *  OKHttp
+     *  ...
+     *@LoadBalanced//如果想要使用注册中心的注册列表访问对应的服务，必须要开启本注解。
+     *  1.采用服务名称访问：如果不加注解，即便你注册了服务，也不能使用名称
+     *  2.开启负载均衡
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
